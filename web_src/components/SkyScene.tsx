55635a0ee83d5d93e8588b89aa1fda346dc7fc68
offpiste111 <jishaochengduo111@gmail.com>
2026-05-18 21:25:17 +0900

    feat: double the amount of clouds in sky background

--- web_src/components/SkyScene.tsx
+++ web_src/components/SkyScene.tsx
@@ -38,6 +38,31 @@ const MovingClouds = () => {
                 segments={20}
                 position={[-20, -5, 10]}
             />
+            {/* Added 3 more clouds to double the amount */}
+            <Cloud
+                opacity={0.5}
+                speed={0.4}
+                bounds={[15, 2, 2]}
+                volume={12}
+                segments={25}
+                position={[-15, -12, -10]}
+            />
+            <Cloud
+                opacity={0.5}
+                speed={0.4}
+                bounds={[18, 2, 2]}
+                volume={14}
+                segments={28}
+                position={[0, -8, 20]}
+            />
+            <Cloud
+                opacity={0.5}
+                speed={0.4}
+                bounds={[12, 2, 2]}
+                volume={10}
+                segments={20}
+                position={[15, -6, -15]}
+            />
         </group>
     );
 };
